2acbb8de2b833da5f25b2244197713b50ada37c9
Andre Macedo <aa.andremacedo@gmail.com>
2022-05-24 17:17:23 -0300

    Requisito 1: corrige erro no arquivo store.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,15 +14,12 @@ class Login extends React.Component {
   validityEmail = (email) => {
     // https://www.horadecodar.com.br/2020/09/13/como-validar-email-com-javascript/
     const response = /\S+@\S+\.\S+/;
-    console.log(response.test(email));
     this.setState({ isValidEmail: response.test(email) });
   }
 
   validityName = (name) => {
     const min = 0;
     const response = name.length > min;
-    console.log(response);
-    console.log(name);
     this.setState({ isValidName: response });
   }
 
