c220c50980a136a3411008e4fc3f3f1a4cf565e3
Andre Macedo <aa.andremacedo@gmail.com>
2022-05-25 21:43:06 -0300

    Adiciona requisito 6

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,92 +1,134 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { getQuestions } from '../redux/actions/gameActions';
+// import { connect } from 'react-redux';
+import './Game.css';
+// import { getQuestions } from '../redux/actions/gameActions';
 
 class Game extends Component {
   constructor() {
     super();
     this.state = {
       questions: [],
+      qIndex: 0,
+      // cIndex: [],
+      // loading: false,
     };
   }
 
   componentDidMount() {
-    const { addQuestions } = this.props;
-    addQuestions();
+    // const { addQuestions } = this.props;
+    // addQuestions();
+    this.fetchQuestion();
   }
 
-  // validateToken = (responseCode) => {
-  //   const { history } = this.props;
-  //   const wrongCode = 3;
-  //   if (responseCode === wrongCode) {
-  //     localStorage.setItem('token', '');
-  //     history.push('/');
-  //   }
-  // }
-
-  // fetchQuestion = async () => {
-  //   const token = localStorage.getItem('token');
-  //   const url = `https://opentdb.com/api.php?amount=5&token=${token}`;
-  //   const response = await fetch(url);
-  //   const apiData = await response.json();
-  //   this.validateToken(apiData.response_code);
-  //   this.setState({ questions: apiData.results });
-  // }
-
-  checkGlobalState = () => {
-    const { questions } = this.props;
-    console.log(questions);
+  validateToken = (responseCode) => {
+    const { history } = this.props;
+    const wrongCode = 3;
+    if (responseCode === wrongCode) {
+      localStorage.setItem('token', '');
+      history.push('/');
+    }
   }
+
+  fetchQuestion = async () => {
+    // this.setState({ loading: true });
+    const token = localStorage.getItem('token');
+    const url = `https://opentdb.com/api.php?amount=5&token=${token}`;
+    const response = await fetch(url);
+    const apiData = await response.json();
+    this.validateToken(apiData.response_code);
+    this.setState({ questions: apiData.results });
+    // this.setState({ loading: false });
+  }
+
+  // checkGlobalState = () => {
+  //   const { questions } = this.props;
+  // }
+
+  // checkLocalState = () => {
+  //   const { questions } = this.state;
+  //   console.log(questions);
+  // }
+
+  getAnswers = () => {
+    // https://www.horadecodar.com.br/2021/05/10/como-embaralhar-um-array-em-javascript-shuffle/
+    function shuffleArray(arr) {
+      for (let i = arr.length - 1; i > 0; i -= 1) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
+      return arr;
+    }
+
+    const { questions } = this.state;
+    const allAnswers = questions[0].incorrect_answers;
+    allAnswers.push(questions[0].correct_answer);
+    return shuffleArray(allAnswers);
+  }
+
+  checkAnswer = (answer, correct) => answer === correct
 
   render() {
     // const { questions } = this.props;
-    const { questions } = this.props;
-
+    const { questions, qIndex } = this.state;
     return (
-      <div>
-        <h1>Let Play</h1>
-        {questions.map((question, index) => (
-          // if ( index === 0) { category0 = question }
-          <h1 key={ index } data-testid="question-category">{ question.category }</h1>
-        ))}
-        {questions
-          // ? <h1 data-testid="question-category">{ JSON.stringify(questions) }</h1>
-          ? <h1 data-testid="question-category">{ questions[0].category }</h1>
-          : <h1 data-testid="question-category"> </h1>}
-        <button
-          data-testid="btn-"
-          type="button"
-          onClick={ this.checkGlobalState }
-        >
-          Populate
-        </button>
-      </div>
+      <section>
+        { questions.length > 0
+        && (
+          <div>
+            <h2 data-testid="question-category">{questions[qIndex].category}</h2>
+            <p data-testid="question-text">{questions[qIndex].question}</p>
+            <div data-testid="answer-options">
+              { this.getAnswers().map((answer, index) => (
+                <button
+                  key={ index }
+                  className={
+                    (this.checkAnswer(answer, questions[qIndex].correct_answer))
+                      ? ''
+                      : 'red'
+                  }
+                  data-testid={
+                    this.checkAnswer(answer, questions[qIndex].correct_answer)
+                      ? 'correct-answer'
+                      : `wrong-answer-${index}`
+                  }
+                  type="button"
+                  onClick={
+                    () => this.checkAnswer(answer, questions[qIndex].correct_answer)
+                  }
+                >
+                  { answer }
+                </button>
+              ))}
+            </div>
+          </div>)}
+      </section>
     );
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  addQuestions: () => {
-    dispatch(getQuestions());
-  },
-  // addQuestions: () => {
-  //   dispatch(getQuestion());
-  // },
-});
+// const mapDispatchToProps = (dispatch) => ({
+//   // addQuestions: () => {
+//   //   dispatch(getQuestions());
+//   // },
+//   // addQuestions: () => {
+//   //   dispatch(getQuestion());
+//   // },
+// });
 
-const mapStateToProps = (state) => ({
-  questions: state.game.questions,
-  currentQuestion: state.game.currentQuestion,
-});
+// const mapStateToProps = (state) => ({
+//   // questions: state.game.questions,
+//   // currentQuestion: state.game.currentQuestion,
+// });
 
 Game.propTypes = {
   history: propTypes.shape({
     push: propTypes.func,
   }).isRequired,
-  addQuestions: propTypes.func.isRequired,
-  questions: propTypes.arrayOf.isRequired,
+  // addQuestions: propTypes.func.isRequired,
+  // questions: propTypes.arrayOf.isRequired,
   // currentQuestion: propTypes.arrayOf.isRequired,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Game);
+export default Game;
+// export default connect(mapStateToProps, mapDispatchToProps)(Game);
--- src/redux/actions/gameActions.js
+++ src/redux/actions/gameActions.js
@@ -1,29 +1,29 @@
-const addQuestions = (payload) => ({
-  type: 'ADD_QUESTIONS',
-  payload,
-});
+// const addQuestions = (payload) => ({
+//   type: 'ADD_QUESTIONS',
+//   payload,
+// });
 
-export const addCurrentQuestions = (payload) => ({
-  type: 'ADD_CURRENT_QUESTION',
-  payload,
-});
+// export const addCurrentQuestions = (payload) => ({
+//   type: 'ADD_CURRENT_QUESTION',
+//   payload,
+// });
 
-const validateToken = (responseCode) => {
-  const { history } = this.props;
-  const wrongCode = 3;
-  if (responseCode === wrongCode) {
-    localStorage.setItem('token', '');
-    history.push('/');
-  }
-};
+// const validateToken = (responseCode) => {
+//   const { history } = this.props;
+//   const wrongCode = 3;
+//   if (responseCode === wrongCode) {
+//     localStorage.setItem('token', '');
+//     history.push('/');
+//   }
+// };
 
-export const getQuestions = () => async (dispatch) => {
-  const token = localStorage.getItem('token');
-  const url = `https://opentdb.com/api.php?amount=5&token=${token}`;
-  const response = await fetch(url);
-  const apiData = await response.json();
-  // validateToken(apiData.response_code);
-  dispatch(addQuestions(apiData.results));
-};
+// export const getQuestions = () => async (dispatch) => {
+//   const token = localStorage.getItem('token');
+//   const url = `https://opentdb.com/api.php?amount=5&token=${token}`;
+//   const response = await fetch(url);
+//   const apiData = await response.json();
+//   // validateToken(apiData.response_code);
+//   dispatch(addQuestions(apiData.results));
+// };
 
 export default getQuestions;
