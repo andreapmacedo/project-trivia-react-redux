6733e0b9aa687cc8d3ca9219d31bc43372838d67
misaelucas <misaelucas@gmail.com>
2022-06-02 05:39:57 -0300

    req18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Settings extends Component {
   render() {
     return (
-      <h1 data-testid="ranking-title">Ranking</h1>
+      <>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="btn-go-home"
+
+          >
+            Início
+          </button>
+        </Link>
+      </>
     );
   }
 }
